c8816d4427bc8de64dbfd5530f39eff5b5a0d78d
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2025-06-13 17:36:01 +0900

    Time: 65 ms (5.11%), Space: 94.9 MB (18.83%) - LeetHub

---
+++ LeetCode/Medium/0077-combinations/0077-combinations.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        backTrack(ans, new ArrayList<>(), 1, n, k);
+        return ans;
+    }
+
+    void backTrack(List<List<Integer>> ans, List<Integer> cur, int start, int n, int k) {
+        if (cur.size() == k) {
+            ans.add(new ArrayList<>(cur));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            if (!cur.contains(i)) {
+                cur.add(i);
+                backTrack(ans, cur, i, n, k);
+                cur.remove(cur.size() - 1);
+            }
+        }
+    }
+}
