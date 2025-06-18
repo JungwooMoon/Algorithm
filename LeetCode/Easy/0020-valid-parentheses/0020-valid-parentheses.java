c211a8ab7ed0c61daf2f77250a6736a0e874daa5
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2025-06-18 21:02:49 +0900

    Time: 3 ms (40.62%), Space: 41.8 MB (65.58%) - LeetHub

---
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(')');
+            } else if (s.charAt(i) == '{') {
+                stack.push('}');
+            } else if (s.charAt(i) == '[') {
+                stack.push(']');
+            } else {
+                if (!stack.isEmpty() && stack.peek() == s.charAt(i)) {
+                    stack.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        if (!stack.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+}
