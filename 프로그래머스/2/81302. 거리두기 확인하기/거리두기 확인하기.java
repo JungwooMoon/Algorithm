c499e436c6e4fb367f69f3ea17358663b3944a8e
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2025-06-21 15:05:58 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.32 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[][] places) {
+        int[] answer = new int[5];
+        boolean pass;
+        for(int u = 0; u < 5; u++) {
+            String[] place = places[u];
+            pass = true;
+            for (int i = 0; i < 5; i++) {
+                if (!pass) break;
+                if (place[i].contains("P")) {
+                    for (int j = 0; j < 5; j++) {
+                       if (place[i].charAt(j) == 'P') {
+                           if (!isValid(i, j, place)) {
+                               pass = false;
+                               break;
+                           }
+                       } 
+                    }
+                }
+            }
+            if (pass) answer[u] = 1; 
+        }
+        
+        return answer;
+    }
+    
+    boolean isValid(int x, int y, String[] place) {
+        Queue<int []> queue = new ArrayDeque<>();
+        queue.offer(new int[] {x, y, 0, 0});
+        
+        boolean[][] visited = new boolean[5][5];
+        visited[x][y] = true;
+        
+        int[] dx = {0, 1, -1, 0};
+        int[] dy = {1, 0, 0, -1};
+        
+        while(!queue.isEmpty()) {
+            int cur[] = queue.poll();
+            
+            if (cur[2] == 2) continue;
+            
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+                
+                if (nx >= 0 && ny >= 0 && nx < 5 && ny < 5) {
+                    if (!visited[nx][ny]) {
+                        if (place[nx].charAt(ny) == 'P') {
+                            if (cur[3] == 0) return false;
+                        } else if (place[nx].charAt(ny) == 'X') {
+                            queue.offer(new int[] {nx, ny, cur[2] + 1, 1});    
+                        } else {
+                            queue.offer(new int[] {nx, ny, cur[2] + 1, 0});   
+                        }
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+}
