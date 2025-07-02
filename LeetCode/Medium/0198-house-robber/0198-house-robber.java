b4ab887d62e7d90e78ad4aeb423d4f50844e4a52
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2025-07-02 22:45:09 +0900

    Time: 0 ms (100%), Space: 41 MB (76.46%) - LeetHub

--- LeetCode/Medium/0198-house-robber/0198-house-robber.java
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.java
@@ -1,19 +1,14 @@
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        if (n == 1) return nums[0];
-        int[] dp = new int[n];
-        Arrays.fill(dp, 0);
+        if (nums.length == 1) return nums[0];
+        int[] dp = new int[nums.length];
         dp[0] = nums[0];
-        dp[1] = nums[1];
-        if (n == 2) return nums[0] > nums[1] ? nums[0] : nums[1];
-        dp[2] = nums[0] + nums[2];
-        for (int i = 3; i < n; i++) {
-            dp[i] = Math.max(dp[i-2] + nums[i], dp[i-3] + nums[i]);
+        dp[1] = Math.max(dp[0], nums[1]);
+
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
         }
-        if (dp[n-1] >= dp[n-2]) return dp[n-1];
 
-        return dp[n-2] ;
-
+        return dp[nums.length-1];
     }
 }
