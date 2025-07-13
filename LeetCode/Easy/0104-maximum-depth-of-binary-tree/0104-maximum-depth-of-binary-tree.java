cedf96f2c7c9a3ac9f1786b1d6be8126ea36bcac
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2025-07-13 15:50:06 +0900

    Time: 0 ms (100%), Space: 43.1 MB (17.09%) - LeetHub

--- LeetCode/Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ LeetCode/Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -18,11 +18,6 @@ class Solution {
 
         if (root == null) return 0;
 
-        if ((root.left == null && root.right == null)) return 1;
-
-        if (root.left == null) return maxDepth(root.right) + 1;
-        if (root.right == null) return maxDepth(root.left) + 1;
-        
         return Math.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1);
     }
 }
