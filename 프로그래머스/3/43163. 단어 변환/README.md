f1a92d57ca53392288c887b894a144b509405adb
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2025-07-25 21:40:56 +0900

    [level 3] Title: 단어 변환, Time: 0.51 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
 
 ### 성능 요약
 
-메모리: 74.9 MB, 시간: 17.22 ms
+메모리: 82.1 MB, 시간: 0.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 25일 15:00:53
+2025년 07월 25일 21:40:23
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,36 +2,36 @@ import java.util.*;
 
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        boolean test = false;
-        for (int i = 0; i < words.length; i++) {
-            if (words[i].equals(target)) test = true;
-        }
-        if (!test) return 0;
-        
+        int answer = 0;
         Queue<String> queue = new ArrayDeque<>();
         Queue<Integer> count = new ArrayDeque<>();
-        boolean[] visited = new boolean[words.length];
+        boolean[] visited = new boolean[words.length+1];
         queue.offer(begin);
         count.offer(0);
         while(!queue.isEmpty()) {
             String cur = queue.poll();
-            int cur_count = count.poll();
-            if (cur.equals(target)) return cur_count;
+            int cnt = count.poll();
+            if (cur.equals(target)) return cnt;
+            
             for (int i = 0; i < words.length; i++) {
-                int simillar = 0;
-                for (int j = 0; j < cur.length(); j++) {
-                    if (words[i].charAt(j) == cur.charAt(j)) simillar++;
-                }
-                if (simillar == cur.length() - 1) {
-                    if (!visited[i]) {
-                        queue.offer(words[i]);
-                        count.offer(cur_count + 1);
-                    }
+                String word = words[i];
+                if(isPossible(cur, word) && !visited[i]) {
+                    visited[i] = true;
+                    queue.offer(word);
+                    count.offer(cnt + 1);
                 }
             }
         }
-        
         return 0;
     }
     
+    boolean isPossible(String x, String y) {
+        int cnt = 0;
+        for (int i = 0; i < x.length(); i++) {
+            if (x.charAt(i) == y.charAt(i)) cnt++;
+        }
+        
+        if (cnt == x.length()-1) return true;
+        return false;
+    }
 }
