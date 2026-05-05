c381ec16a35c06229d009a7ade68c0e1328a24b9
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2026-05-05 22:52:31 +0900

    Time: 5 ms (15.11%), Space: 43.2 MB (72.36%) - LeetHub

--- LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.java
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,27 +1,29 @@
-import java.util.Stack;
+import java.util.*;
 
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                stack.push(')');
-            } else if (s.charAt(i) == '{') {
-                stack.push('}');
-            } else if (s.charAt(i) == '[') {
-                stack.push(']');
-            } else {
-                if (!stack.isEmpty() && stack.peek() == s.charAt(i)) {
-                    stack.pop();
-                }
-                else {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
                     return false;
                 }
+            } else if (c == '}') {
+                if (stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            } else if (c == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            } else {
+                stack.push(c);
             }
         }
-        if (!stack.isEmpty()) {
-            return false;
-        }
-        return true;
+
+        if (stack.isEmpty()) return true;
+
+        return false; 
     }
 }
