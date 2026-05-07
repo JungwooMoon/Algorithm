df5630fc15e2110a0467d29fdc8adadec59f46b0
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2026-05-08 01:19:27 +0900

    Time: 2 ms (53.96%), Space: 46 MB (95.22%) - LeetHub

--- LeetCode/Medium/0841-keys-and-rooms/0841-keys-and-rooms.java
+++ LeetCode/Medium/0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,31 +1,26 @@
+import java.util.*;
+
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-				//✅ 각 방의 방문 여부를 기록할 리스트를 선언한다.
+        Queue<Integer> queue = new ArrayDeque<>();
+
         boolean[] visited = new boolean[rooms.size()];
-        BFS(rooms, visited, 0);
-        for (int i = 0; i < visited.length; i++) {
-            if (!visited[i]) {
-                return false;
+
+        queue.offer(0);
+        visited[0] = true;
+        int visit = 1;
+        while(!queue.isEmpty()) {
+            int cur = queue.poll();
+
+            for (int key : rooms.get(cur)) {
+                if (!visited[key]) {
+                    queue.offer(key);
+                    visited[key] = true;
+                    visit++;
+                }
             }
         }
-        return true;
-    }
-    public void BFS(List<List<Integer>> rooms, boolean[] visited, int start) {
-        Queue<Integer> queue = new ArrayDeque<Integer>();
-        //int count = 0;
-        queue.offer(start);
-        visited[start] = true;
-        //count++;
-        while(!queue.isEmpty()) {
-            int current = queue.poll();
-            for (int room : rooms.get(current)) {
-                if (!visited[room]) {
-                    queue.offer(room);
-                    visited[room] = true;
-                    //count++;
-                }
-            }        
-        }
-        //return count;
+
+        return rooms.size() == visit;
     }
 }
