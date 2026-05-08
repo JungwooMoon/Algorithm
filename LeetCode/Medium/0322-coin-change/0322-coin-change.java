1a1f7a2b57604dfb49f921cf36c387ae2e70799a
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2026-05-08 12:34:39 +0900

    Time: 96 ms (5%), Space: 46.2 MB (70.81%) - LeetHub

--- LeetCode/Medium/0322-coin-change/0322-coin-change.java
+++ LeetCode/Medium/0322-coin-change/0322-coin-change.java
@@ -1,20 +1,19 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int dp[] = new int[10001];
-
+        int[] dp = new int[amount+1];
+        dp[0] = 0;
         for (int i = 1; i <= amount; i++) {
-            dp[i] = amount+1;
+            dp[i] = amount + 1;
         }
 
         for (int i = 1; i <= amount; i++) {
             for (int coin : coins) {
                 if (i >= coin) {
-                    dp[i] = Math.min(dp[i-coin] + 1, dp[i]);
+                    dp[i] = Math.min(dp[i], dp[i-coin] + 1);
                 }
             }
         }
 
-        if (dp[amount] > amount) return -1;
-        return dp[amount];
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
