680ad17b3605d7f21d168943fa6d79621bac0aff
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2026-05-08 23:29:56 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT o.animal_id, o.animal_type, o.name
+from animal_ins i join animal_outs o 
+on i.animal_id = o.animal_id
+where i.sex_upon_intake like 'Intact%' and
+(o.sex_upon_outcome Like 'Neutered%' or o.sex_upon_outcome Like 'Spayed%') 
+order by o.animal_id
