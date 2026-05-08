c637d0fceaf907b707a46535f6eee2a68477d663
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2026-05-08 16:29:47 +0900

    Time: 21 ms (20.48%), Space: 47.5 MB (80.17%) - LeetHub

--- LeetCode/Medium/1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ LeetCode/Medium/1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -2,37 +2,38 @@ import java.util.*;
 
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        if (grid[0][0] == 1 || grid[grid.length-1][grid.length-1] == 1) {
-            return -1;
-        }
-        
-        int dx[] = new int[] {0, 0, 1, 1, 1, -1, -1, -1};
-        int dy[] = new int[] {1, -1, 0, 1, -1, 0, 1, -1};
+        if (grid[0][0] == 1) return -1;
 
-        Queue<int []> queue = new ArrayDeque<>();
-        queue.offer(new int[] {0, 0, 1});
-        grid[0][0] = 1;
-        while(!queue.isEmpty()) {
+        int n = grid.length;
+
+        int[] dx = {0, 0, 1, 1, 1, -1, -1, -1};
+        int[] dy = {1, -1, 0, 1, -1, 1, -1, 0};
+
+        Queue<int[]> queue = new ArrayDeque<>();
+        boolean[][] visited = new boolean[n][n];
+
+        queue.offer(new int[] { 0, 0, 1 });
+        visited[0][0] = true;
+
+         while(!queue.isEmpty()) {
             int[] cur = queue.poll();
-
-            if (cur[0] == grid.length - 1 && cur[1] == grid.length -1) return cur[2];
+            
+            if (cur[0] == n-1 && cur[1] == n-1) return cur[2];
 
             for (int i = 0; i < 8; i++) {
                 int nx = cur[0] + dx[i];
                 int ny = cur[1] + dy[i];
 
-                if (isValid(nx, ny, grid.length)) {
-                    if (grid[nx][ny] == 0) {
-                        grid[nx][ny] = 1;
-                        queue.offer(new int[] {nx, ny, cur[2] + 1});
-                    } 
-                }
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+                    if (!visited[nx][ny] && grid[nx][ny] == 0) {
+                        queue.offer(new int[] {nx, ny, cur[2]+1});
+                        visited[nx][ny] = true;
+                    }
+                }   
             }
         }
-        return -1;
-    }
 
-    boolean isValid(int x, int y, int n) {
-        return x >= 0 && y >= 0 && x < n && y < n;
+
+        return -1;  
     }
 }
