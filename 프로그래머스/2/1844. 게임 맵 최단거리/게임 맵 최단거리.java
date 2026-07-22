bbe9dd0d28e1746e54670f1fbcda47d757da2a2c
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2026-07-22 19:22:37 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.21 ms, Memory: 57.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,37 +2,41 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] maps) {
-        Queue<int []> queue = new ArrayDeque<> ();
-        boolean[][] visited = new boolean[maps.length][maps[0].length];
+        
+        if(maps[0][0] == 0) return -1;
+        
+        int n = maps.length;
+        int m = maps[0].length;
+        
+        boolean[][] visited = new boolean[n][m];
+        
+        Queue<int[]> queue = new ArrayDeque<>();
+        
         queue.offer(new int[] {0, 0, 1});
         
-        int[] dx = {0, -1, 1, 0};
-        int[] dy = {1, 0, 0, -1};
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+        
+        visited[0][0] = true;
+        
         while(!queue.isEmpty()) {
             int[] cur = queue.poll();
-            if (cur[0] == maps.length-1 && cur[1] == maps[0].length-1) {
-                return cur[2];
-            }
             
-            for (int i = 0; i < 4; i++) {
-                int n_x = cur[0] + dx[i];
-                int n_y = cur[1] + dy[i];
-                if (isValid(n_x, n_y, maps.length, maps[0].length)) {
-                    if (maps[n_x][n_y] == 1 && !visited[n_x][n_y]) {
-                        queue.offer(new int[] { n_x, n_y, cur[2]+1 });
-                        visited[n_x][n_y] = true;
+            if (cur[0] == n-1 && cur[1] == m-1) return cur[2];
+            
+            for(int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+                
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && maps[nx][ny] == 1) {
+                    if (!visited[nx][ny]) {
+                        queue.offer(new int[] {nx, ny, cur[2]+1});
+                        visited[nx][ny] = true;
                     }
                 }
             }
-            
-            
         }
         
-        
         return -1;
-    }
-    
-    boolean isValid(int x, int y, int lengthX, int lengthY) {
-        return (x >= 0 && x < lengthX) && (y >= 0 && y < lengthY); 
     }
 }
