ba357506ba19cd352038ddcc1050aae1cbe613f7
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2026-07-22 19:48:17 +0900

    [level 3] Title: 단어 변환, Time: 0.19 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,40 +2,43 @@ import java.util.*;
 
 class Solution {
     public int solution(String begin, String target, String[] words) {
+        
+        boolean[] visited = new boolean[words.length];
+        
         Queue<String> queue = new ArrayDeque<>();
         Queue<Integer> cnt = new ArrayDeque<>();
+
         queue.offer(begin);
         cnt.offer(0);
-        boolean[] visited = new boolean[words.length];
+        
         while(!queue.isEmpty()) {
             String cur = queue.poll();
-            int cur_cnt = cnt.poll();
-            for (int i = 0; i < words.length; i++) {
-                if (check(cur, words[i]) && !visited[i]) {
-                    queue.offer(words[i]);
-                    visited[i] = true;
-                    cnt.offer(cur_cnt+1);
-                    if (target.equals(words[i])) return cur_cnt+1;
-                } 
-            }
+            int curCnt = cnt.poll();
             
+            if (cur.equals(target)) return curCnt;
+            
+            for(int i = 0; i < words.length; i++) {
+                String word = words[i];
+                if (isPossible(word, cur) && !visited[i]) {
+                    queue.offer(word);
+                    cnt.offer(curCnt+1);
+                    visited[i] = true;
+                }
+            }
         }
-        
-        
-        
         
         return 0;
     }
     
-    boolean check(String a, String b) {
+    boolean isPossible(String a, String b) {
+        int cnt = 0;
         
-        int n = 0;
-        
-        for (int i = 0;  i < a.length(); i++) {
-            if (a.charAt(i) == b.charAt(i)) n++;
+        for(int i = 0; i < a.length(); i++) {
+            if(a.charAt(i) == b.charAt(i)) {
+                cnt++;
+            }
         }
         
-        return n == a.length() - 1;
-        
+        return cnt == a.length()-1;
     }
 }
