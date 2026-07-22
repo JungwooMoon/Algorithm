c5b807fa145a050819875fd6f85cc315cdbc1920
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2026-07-22 15:12:08 +0900

    [level 3] Title: 양과 늑대, Time: 8.06 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,68 +1,66 @@
 import java.util.*;
 
 class Solution {
+    
     public int solution(int[] info, int[][] edges) {
-        Map<Integer, Node> tree = new HashMap<>();
+        
+        Map<Integer, List<Integer>> tree = new HashMap<>();
         
         for (int i = 0; i < info.length; i++) {
-            tree.put(i, new Node());
+            tree.put(i, new ArrayList<>());
         }
         
         for (int[] edge : edges) {
-            if (tree.get(edge[0]).left >= 0) tree.get(edge[0]).right = edge[1];
-            else tree.get(edge[0]).left = edge[1];
-        }
-    
-        int[] max = dfs(info, new int[] {0, 0, 0}, tree);
-        return max[0];
-    }
-    
-    int[] dfs(int[] info, int[] cur, Map<Integer, Node> tree) {
-        int wolf = cur[1];
-        int sheep = cur[0];
-        if (info[cur[2]] == 0) sheep++;
-        else wolf++;
-        if (sheep <= wolf) return cur;
-        
-        Node curNode = tree.get(cur[2]);
-        int[] cnt1 = new int[3];
-        int[] cnt2 = new int[3];
-        
-        if (tree.get(cur[2]).left < 0) return new int[] {sheep, wolf, 0};
-        cnt1 = dfs(info, new int[] {sheep, wolf, curNode.left}, tree);
-        if (tree.get(cur[2]).right >= 0) cnt1 = dfs(info, new int[] {cnt1[0], cnt1[1], curNode.right}, tree);
-        
-        if (tree.get(cur[2]).right >= 0) cnt2 = dfs(info, new int[] {sheep, wolf, curNode.right}, tree);
-        cnt2 = dfs(info, new int[] {cnt2[0], cnt2[1], curNode.left}, tree);
-        
-        
-        int max = Math.max(cnt1[0], cnt2[0]);
-        
-        while(max != sheep) {
-            sheep = max;
-           if (tree.get(cur[2]).left < 0) return new int[] {sheep, wolf, 0};
-            cnt1 = dfs(info, new int[] {sheep, wolf, curNode.left}, tree);
-            if (tree.get(cur[2]).right >= 0) cnt1 = dfs(info, new int[] {cnt1[0], cnt1[1], curNode.right}, tree);
-
-            if (tree.get(cur[2]).right >= 0) cnt2 = dfs(info, new int[] {sheep, wolf, curNode.right}, tree);
-            cnt2 = dfs(info, new int[] {cnt2[0], cnt2[1], curNode.left}, tree);
-
-
-            max = Math.max(cnt1[0], cnt2[0]); 
+            tree.get(edge[0]).add(edge[1]);    
         }
         
-        return new int[] {max, wolf, 0};
-         
+        Set<Integer> nexts = new HashSet<>();
+        
+        for (int n : tree.get(0)) {
+            nexts.add(n);
+        }
+        
+        int answer = dfs(1, 0, nexts, tree, info);
+        
+        return answer;
     }
     
-    class Node {
-        int left;
-        int right;
+    int dfs(int sheep, int wolf, Set<Integer> nexts, Map<Integer, List<Integer>> tree, int[] info) {
+       int max = sheep;
         
-        Node() {
-            left = -1;
-            right = -1;
-        }
+        for (int next : nexts) {
+            List<Integer> candidate = tree.get(next);
+            
+            Set<Integer> nextCandidate = new HashSet<>(nexts);
+            nextCandidate.remove(next);
+            
+            int n_sheep = sheep;
+            int n_wolf = wolf;
+            
+            if (info[next] == 0) {
+                n_sheep++;
+            } else {
+                n_wolf++;
+                if (n_sheep == n_wolf) continue;
+            }
+
+            for (int can : candidate) {
+                nextCandidate.add(can);
+            }
+
+            max = Math.max(dfs(n_sheep, n_wolf, nextCandidate, tree, info), max);
+            
+            for (int can : candidate) {
+                nextCandidate.remove(can);
+            }
+            
+        } 
+        
+        return max;
+        
     }
+    
+    
+    
     
 }
