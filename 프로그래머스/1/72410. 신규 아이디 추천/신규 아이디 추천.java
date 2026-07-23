ac46312a09b4ea25eb8c0acb88b62ebd50aa8c14
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2026-07-23 15:44:24 +0900

    [level 1] Title: 신규 아이디 추천, Time: 9.75 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,55 +1,69 @@
 class Solution {
     public String solution(String new_id) {
-        String answer = "";
-        String lower_id = new_id.toLowerCase();
-        String second = "";
-        
-        for (int i = 0; i < lower_id.length(); i++) {
-            if ((lower_id.charAt(i) >= 'a' && lower_id.charAt(i) <= 'z') || (lower_id.charAt(i) >= '0' && lower_id.charAt(i) <= '9') || lower_id.charAt(i) == '-' || lower_id.charAt(i) == '_' || lower_id.charAt(i) == '.') {
-      second += lower_id.charAt(i);     
-            } 
-        }
-        
-        String third = "";
-        
-        for (int i = 0; i < second.length(); i++) {
-            if (second.charAt(i) == '.') {
-                third += second.charAt(i);
-                while (i + 1 < second.length() && second.charAt(i+1) == '.') {
-                    i++;
-                } 
-            } else {
-                third += second.charAt(i);
+        String step1 = new_id.toLowerCase(); 
+        String step2 = "";
+        for(int i = 0; i < step1.length(); i++) {
+            if (step1.charAt(i) >= 'a' && step1.charAt(i) <= 'z') {
+                step2 += step1.charAt(i);
+            }
+            
+            if (step1.charAt(i) >= '0' && step1.charAt(i) <= '9') {
+                step2 += step1.charAt(i);
+            }
+            
+            if (step1.charAt(i) == '-' || step1.charAt(i) == '_' || step1.charAt(i) == '.') {
+                step2 += step1.charAt(i);
             }
         }
         
-        String remove_start = third;
-        
-        if (remove_start.startsWith(".")) {
-            remove_start = remove_start.substring(1);
+        String step3 = "";
+        int cnt = 0;
+        for (int i = 0; i < step2.length(); i++) {
+            if (step2.charAt(i) == '.' && cnt == 0) {
+                step3 += step2.charAt(i);
+                cnt++;
+            }
+            
+            if (step2.charAt(i) != '.') {
+                step3 += step2.charAt(i);
+                cnt = 0;
+            }
         }
         
-        if (remove_start.endsWith(".")) {
-            remove_start = remove_start.substring(0, remove_start.length()-1);
-        } 
+        String step4 = step3;
         
-        if (remove_start.equals("")) {
-            answer = "a";
-        } else {
-            answer = remove_start;
+        if (step4.startsWith(".")) {
+            step4 = step4.substring(1, step4.length());
         }
         
-        if (answer.length() >= 16) {
-            answer = answer.substring(0, 15); 
+        if (step4.endsWith(".")) {
+            step4 = step4.substring(0, step4.length()-1);
         }
         
-        if (answer.endsWith(".")) {
-            answer = answer.substring(0, answer.length() - 1);
+        String step5 = step4;
+        
+        if(step5.length() == 0) {
+            step5 += 'a';
         }
         
-        while(answer.length() <= 2) {
-            answer += answer.charAt(answer.length()-1);
+        String step6 = step5;
+        
+        if (step6.length() > 15) {
+            step6 = step6.substring(0, 15);
         }
+        
+        if (step6.endsWith(".")) {
+            step6 = step6.substring(0, step6.length()-1);
+        }
+        
+        String answer = step6;
+        
+        if (answer.length() <= 2) {
+            while(answer.length() < 3) {
+                answer += answer.charAt(answer.length()-1);
+            }
+        }
+        
         
         return answer;
     }
